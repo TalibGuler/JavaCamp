4f72226de8960ee1bf04a76589e4b7cd5fa2af6f
TalibGuler <64988496+TalibGuler@users.noreply.github.com>
2021-05-02 02:50:59 +0300

    01-05-2021

--- inheritance2/src/inheritance2/DataBaseLogger.java
+++ inheritance2/src/inheritance2/DataBaseLogger.java
@@ -1,7 +1,7 @@
 package inheritance2;
 
 public class DataBaseLogger extends Logger {
-	@Override
+	@Override // fonksiyonu deðiþtirme
 	public void log() {
 		System.out.println("Database loglandý.");
 	}
